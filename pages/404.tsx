31770c481bbb908d96f276a4489117efe195522a
joao vitor v <joaovitorzv@outlook.com>
2021-10-11 19:57:39 -0300

    feat: custom 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,21 @@
-import { GetStaticProps } from "next";
+import Head from 'next/head'
+import Header from '../components/header'
+
+import NotFoundStyles from '../styles/404.module.css'
 
 function Custom404() {
-  return null
-}
-
-export const getStaticProps: GetStaticProps = () => {
-  return {
-    redirect: {
-      destination: '/',
-      permanent: true
-    }
-  } 
+  return (
+    <div className='container'>
+      <Head>
+        <title>@joaovitorzv • 404 Not Found</title>
+      </Head>
+      <Header />
+      <main className={NotFoundStyles.content}>
+        <h2>404</h2>
+        <p>Sorry, page not found!</p>
+      </main>
+    </div>
+  ) 
 }
 
 export default Custom404; 
