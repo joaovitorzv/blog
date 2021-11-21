f51eee53c279e1199f4596c0030e964955a01d8e
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 16:14:37 -0300

    fix: files indentation & tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
-import { NextPage, GetStaticProps } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Header from '../components/header'
-import styles from '../styles/Blog.module.css'
+import { NextPage, GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/header";
+import styles from "../styles/Blog.module.css";
 
-import { formatDate } from '../utils'
+import { formatDate } from "../utils";
 
-import { gql } from '@apollo/client'
-import client from '../graphql-client'
+import { gql } from "@apollo/client";
+import client from "../graphql-client";
 
 type Post = {
-  id: string,
-  title: string,
-  description: string,
-  date: string, 
-  slug: string
-}
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  slug: string;
+};
 type Props = {
-  posts: Post[] 
-}
+  posts: Post[];
+};
 
 const Home: NextPage<Props> = ({ posts }) => {
   return (
-    <div className='container'>
+    <div className="container">
       <Head>
         <title>@joaovitorzv • blog</title>
         <meta name="description" content="My place on internet" />
@@ -33,10 +33,12 @@ const Home: NextPage<Props> = ({ posts }) => {
           <section key={post.title} className={styles.post}>
             <Link href={`/post/${post.slug}`}>
               <a className={styles.postTitle}>{post.title}</a>
-            </Link> 
+            </Link>
             <span>{formatDate(post.date)}</span>
             <p>{post.description}</p>
-            <Link href={`/post/${post.slug}#keep-reading`}>continuar lendo...</Link>
+            <Link href={`/post/${post.slug}#keep-reading`}>
+              continuar lendo...
+            </Link>
           </section>
         ))}
       </main>
@@ -45,27 +47,27 @@ const Home: NextPage<Props> = ({ posts }) => {
         <p>Copyright &copy; new Date().getFullYear() João Vitor Veras </p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await client.query({
     query: gql`
       query BlogPosts {
         posts {
-          id,
-          title,
-          description,
-          date,
+          id
+          title
+          description
+          date
           slug
         }
       }
-    `
+    `,
   });
 
   return {
-    props: data
-  }
-}
+    props: data,
+  };
+};
 
-export default Home
+export default Home;
