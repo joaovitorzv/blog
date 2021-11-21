887506acee4f170de75c750a57058aeb8277984a
joao vitor v <joaovitorzv@outlook.com>
2021-11-21 15:04:10 -0300

    test indentation

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,18 +4,18 @@ import Header from "../components/header";
 import NotFoundStyles from "../styles/404.module.css";
 
 function Custom404() {
-  return (
-    <div className="container">
-      <Head>
-        <title>@joaovitorzv • 404 Not Found</title>
-      </Head>
-      <Header />
-      <main className={NotFoundStyles.content}>
-        <h2>404</h2>
-        <p>Deculpe, não encontrei nada aqui!</p>
-      </main>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Head>
+				<title>@joaovitorzv • 404 Not Found</title>
+			</Head>
+			<Header />
+			<main className={NotFoundStyles.content}>
+				<h2>404</h2>
+				<p>Deculpe, não encontrei nada aqui!</p>
+			</main>
+		</div>
+	);
 }
 
 export default Custom404;
