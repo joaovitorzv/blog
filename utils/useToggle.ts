9ba9689357eed7d74ed3e64d97e508af6a5ca8e3
João V. Veras <joaovitorzv@outlook.com>
2022-05-10 23:39:52 -0300

    refactor(header): move social links

--- utils/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-export function useToggle(initalState = false): [boolean, any] {
-  const [state, setState] = useState<boolean>(initalState);
-
-  const toggle = useCallback(() => setState((state) => !state), []);
-
-  return [state, toggle];
-}
